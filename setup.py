6a52cda0e8f11c5ef9ec4ce8a80a48dfa035ddd9
Daniel Greenfeld <pydanny@gmail.com>
2013-06-27 13:30:17 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+def get_author(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__author__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+author = get_author('registration_bootstrap')
+version = get_version('registration_bootstrap')
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(
+    name='django-registration-bootstrap',
+    version=version,
+    description="Bootstrap implementation for Django registration.",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    keywords='django',
+    author=author,
+    author_email='pydanny@gmail.com',
+    url='http://github.com/pydanny/django-registration',
+    license='MIT',
+    packages=get_packages('registration_bootstrap'),
+    include_package_data=True,
+    test_suite='runtests.runtests',
+    install_requires=[
+        'django>=1.5.0',
+        'django-registration>=1.0.0',
+        #'django-floppyforms==1.1',
+        'django-crispy-forms==1.3.2'
+        ],
+    zip_safe=False,
+)
+
+# (*) Please direct queries to Github issue list, rather than to me directly
+#     Doing so helps ensure your question is helpful to other users.
+#     Queries directly to my email are likely to receive a canned response, 
+#       usually asking you to open an issue on Github.
+#
+#     Many thanks for your understanding.
